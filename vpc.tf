e35c015bfd73e0ea4a30cf623f0aaaa63c39a32b
Bob Clough <bob.clough@codethink.co.uk>
2023-04-18 16:19:37 +0100

    We don't want VPC to be randomly suffixed per run

--- vpc.tf
+++ vpc.tf
@@ -13,7 +13,7 @@ resource "random_string" "suffix" {
 
 locals {
   cluster_name     = "cfi-eks-${random_string.suffix.result}"
-  vpc_name         = "cfi-vpc-${random_string.suffix.result}"
+  vpc_name         = "cfi-vpc"
   k8s_cluster_name = "cfi-eks-${random_string.suffix.result}-k8s"
 }
 
